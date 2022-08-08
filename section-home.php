6355e868c84d507384ead33c292b08fc4a61d77b
DESKTOP-5HIPBNU\tj-bu <a@a.net>
2022-08-08 16:24:48 +0900

    폰트추가 김현아

--- section-home.php
+++ section-home.php
@@ -1,9 +1,33 @@
-<section class="section-home section-container  section-container1"  data-section-number="1">
+
+<section class="section-home section-container  section-container1" data-section-number="1">
   <a class="btn-page" href="#home" data-section=".section-home" data-section-number="1">Home</a>
   <section class="section-video-intro">
     <iframe class="video-content" src="https://www.youtube.com/embed/BHACKCNDMW8?&playlist=BHACKCNDMW8&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
     <img class="youtube-size-img" src="./img/icons/560x315.jpg" draggable="false" alt="">
     <img class="white-cover" src="./img/icons/white-cover.png" alt="">
   </section>
-  <section>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br>home<br></section>
-</section>
+  <link rel="stylesheet" href="./css/home.css">
+  <section class="haHomesection">
+    <h1 class="hidden">home</h1>
+    <section class="haSection1">
+      <h2 class="hidden">Making photo</h2>
+      <div class="haHome1">
+        <figure>
+          <img class="home1img" src="./img/home/home1-1.png" alt="">
+        </figure>
+        <figcaption>
+          <h3>Making 123</h3>
+          <p class="home1box">
+            <em>MAKING PHOTOGRAPHY</em> <br>
+              제가 원하는 컨셉의 사진을을 만드는 과정을 담은 영상입니다.
+              여행을 다니며 자연스러운 분위기 속에서 주로 작업하며,
+              사진을 좋아하는 모든 사람들에게 귀감이 되는 작업영상이 되도록 하겠습니다.
+          </p>
+        </figcaption>
+        <div class="haSectionvideo">
+          <a href="https://www.youtube.com/watch?v=s2xF2lm64HM">Making photography</a>
+        </div>
+      </div><!-- home1 -->
+    </section><!-- Sec1 -->
+  </section><!-- homeS -->
+</section><!-- section-home section-container  section-container1 -->
