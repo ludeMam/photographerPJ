2b3269cc24c18266eed7f048a26eda61d3910a0e
bluehalu98 <bluehalu98@gmail.com>
2022-08-09 18:17:45 +0900

    8/9 진영진 작업완료(학원)

--- js/section-about-introduce.js
+++ js/section-about-introduce.js
@@ -1,21 +1,33 @@
 $(function(){
+  var scrS
   var textS
   var textE
   var textRange
+  var textLocation
+  var imgT
+  var meta
   function introTextScrMotion(){
-    textS = $('.yj-introduce-section .profile-text').offset().top + scrt+300
-    textE = $('.yj-introduce-section').offset().top+scrt+$('.yj-introduce-section').innerHeight()
+    scrS=0
+    scrE = $('section.yj-introduce-section').offset().top + scrt 
+    scrRatio= (scrt-scrS)/scrE
+    if(scrRatio>1){scrRatio=1}
+    textS = 0
+    textE = $('section.yj-introduce-section').innerHeight()*0.5
     textRange = textE-textS
-    textRatio= (scrt-textS)/textRange
-    if(textRatio>1){textRatio=1}
-    if(textRatio<0){textRatio=0}
-    console.log(textRatio);
+    textLocation = textRange*(1-scrRatio) + textS
+    $('section.yj-introduce-section div.profile-text').css({'top':textLocation})
+    imgT=$('section.yj-introduce-section figure.profile-img-container img.profile-img').offset().top+scrt
+    imgH=$('section.yj-introduce-section figure.profile-img-container img.profile-img').innerHeight()
+    meta = 0 +(scrt-(imgT-winh*0.5+imgH*0.5)) * -0.1
+    $('section.yj-introduce-section figure.profile-img-container img.profile-img').css({
+      '-webkit-mask-position': 'center '+meta + 'px'
+    })
   }
   introTextScrMotion()
-  $(window).scroll(function(){
-    introTextScrMotion()
-  }).resize(function(){
+  $('main').bind('scroll',function(){
     introTextScrMotion()
   })
-
+  $(window).resize(function(){
+    introTextScrMotion()
+  })
 })
--- section-about.php
+++ section-about.php
@@ -5,27 +5,42 @@
     <img class="youtube-size-img" src="./img/icons/560x315.jpg" draggable="false" alt="">
     <img class="white-cover" src="./img/icons/white-cover.png" alt="">
   </section>
+  <link rel="stylesheet" href="./css/section-about-main.css">
   <link rel="stylesheet" href="./css/section-about-introduce.css">
   <script src="./js/section-about-introduce.js"></script>
+  <section class="yj-main-section">
+    <h2 class="hidden">About Main</h2>
+    <div>
+      <img class="main-background" src="./img/aboutme/mainBackground.png" alt="">
+  <!--     <figure>
+        <img class="mainImg1" src="./img/aboutme/mainImg1.png" alt="">
+        <img class="mainImg2" src="./img/aboutme/mainImg2.png" alt="">
+      </figure> -->
+    </div>
+  </section>
+  <!-- main -->
   <section class="yj-introduce-section">
-    <div class="profile-background">
-      <h2 class="hidden">introduce section</h2>
+    <h2 class="hidden">introduce section</h2>
+    <div class="profile-background">  
       <div class="profile-container">
         <figure class="profile-img-container">
-          <img src="./img/aboutme/profile.png" alt="" class="profile-img">
+          <img src="./img/aboutme/profile1.png" alt="" class="profile-img">
         </figure>
         <figcaption>
           <div class="profile-text">
             <h3>About Me</h3>
             <h4>권선호</h4>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              자연은 항상 제게 신비로움을 보여줬습니다. <br>
+              어떠한 곳을 가보아도 새로운 영감과 놀라움을 선사해주었고, <br>
+              제가 느낀 그 감정들을 사람들에게 전달하고자 사진작가의 길을 걷게 되었습니다. 
             </p>
           </div>
         </figcaption>
       </div>
     </div>
   </section>
+  <!-- introduce -->
   <section>
     about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>about<br>
   </section>
