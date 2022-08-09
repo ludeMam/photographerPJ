b8a87ee506ed67dfe4a30912e0fb9734271f24c3
DESKTOP-5HIPBNU\tj-bu <a@a.net>
2022-08-09 16:48:22 +0900

    메인화면 갈아엎음/김현아

--- section-home.php
+++ section-home.php
@@ -1,36 +1,31 @@
-
 <section class="section-home section-container  section-container1" data-section-number="1">
   <a class="btn-page" href="#home" data-section=".section-home" data-section-number="1">Home</a>
   <section class="section-video-intro">
     <iframe class="video-content" src="https://www.youtube.com/embed/BHACKCNDMW8?&playlist=BHACKCNDMW8&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
     <img class="youtube-size-img" src="./img/icons/560x315.jpg" draggable="false" alt="">
-
   </section>
+
   <link rel="stylesheet" href="./css/home.css">
-  <section class="haHomesection">
-    <h1 class="hidden">home</h1>
-    <section class="haSection1">
-      <h2 class="hidden">Making photo</h2>
-      <div class="haHome1">
-        <figure>
-          <img class="home1img" src="./img/home/home1-1.png" alt="">
-        </figure>
-        <figcaption>
-          <h3>Making</h3>
-          <div class="haex1">
-          <p class="home1box">
-            <em>MAKING PHOTOGRAPHY</em> <br>
-              <i>제가 원하는 컨셉의 사진을을 만드는 과정을 담은 영상입니다.
-              여행을 다니며 자연스러운 분위기 속에서 주로 작업하며,
-              사진을 좋아하는 모든 사람들에게 귀감이 되는 작업영상이 되도록 하겠습니다.
-              </i>
-          </p>
-          </div>
-        </figcaption>
-        <div class="haSectionvideo">
-          <a href="https://www.youtube.com/watch?v=s2xF2lm64HM">Making video</a>
-        </div>
-      </div><!-- home1 -->
-    </section><!-- Sec1 -->
+
+  <section class="ha-Homesection-1">
+    <h2 class="hidden">Making photo</h2>
+    <b class="title">Making</b>
+    <div class="center">
+      <div class="textbox">
+        <h3>
+          <small>MAKE POHTIGRAPHY</small>
+          <strong>MAKE POHTIGRAPHY</strong>
+        </h3>
+        <p>
+        제가 원하는 컨셉의 사진을을 만드는 과정을
+담은 영상입니다. 여행을 다니며 자연스러운 분위기
+속에서 주로 작업하며, 사진을 좋아하는 모든
+사람들에게 귀감이 되는 작업영상이 되도록
+하겠습니다.
+        </p>
+      </div>
+    </div><!--center-->
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>  
   </section><!-- homeS -->
+  
 </section><!-- section-home section-container  section-container1 -->
