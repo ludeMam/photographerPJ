075d4d471b5c89802822f5fedc1fe277fcac9dd5
jslee76 <109638295+jslee76@users.noreply.github.com>
2022-08-10 18:02:41 +0900

    photo1/ css, scss-js

--- section-photo1.php
+++ section-photo1.php
@@ -1,9 +1,100 @@
-<section class="section-photo1 section-container section-container3"  data-section-number="3">
+<link rel="stylesheet" href="./css/section-photo1.css">
+<link rel="stylesheet" href="./css/section-photo1.scss">
+
+<section class="section-photo1 section-container section-container3" data-section-number="3">
   <a class="btn-page" href="#photo1" data-section=".section-photo1" data-section-number="3">Bright moody</a>
-  <section class="section-video-intro">
+
+  <section class="section-video-intro js-section-1">
+    <h6>
+      BRIGHT ADN COLD & <br>
+      HIGHLIGHT PHOTOGRAPHY
+    </h6>
+
     <iframe class="video-content" src="https://www.youtube.com/embed/BHACKCNDMW8?&playlist=BHACKCNDMW8&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
     <img class="youtube-size-img" src="./img/icons/560x315.jpg" draggable="false" alt="">
     <img class="white-cover" src="./img/icons/white-cover.png" alt="">
+
+    <?php include "section-photo2-svg.php" ?>
   </section>
+
+  <section class="js-section-2">
+    <figure>
+      <h6>
+        SCENE & <br>
+        LANDSCAPE2
+      </h6>
+
+      <div class="js-imgcontainer1">
+        <div class="js-img1 img-1-1">
+          <img class="f1" src="./img/photo1/first1.png" >
+          <img class="f2" src="./img/photo1/first2.png" >
+          <div class="text text1-1">
+            <em>Camera, Lighting, and Effects</em>
+            <i>
+              Identify items in the physical environment, including planar surfaces,
+              images, physical objects, body positions, and faces.
+            </i>
+          </div>
+        </div>
+
+        <div class="js-img1 img-1-2">
+          <img class="f3" src="./img/photo1/first3.png">
+          <img class="f4" src="./img/photo1/first4.png">
+          <div class="text text1-2">
+            <em> to Achieve Light Painting</em>
+            <i>
+              You will need a lens that can have its focal length moved manually during
+              long exposures. Abstract photos are possible with exposures of around 2
+              seconds. Longer exposures of 20 or 30 seconds will allow the zoom effect
+              to be combined with a static phase.
+            </i>
+          </div>
+        </div>
+      </div><!-- js-imgcontainer1 -->
+
+      <div class="js-imgcontainer2">
+        <div class="js-img2 img-2-1">
+          <img class="s1" src="./img/photo1/sec1.png" >
+          <img class="s2" src="./img/photo1/sec2.png" >
+        </div>
+        <div class="js-img2 img-2-2">
+          <img class="s3" src="./img/photo1/sec3.png" >
+          <img class="s4" src="./img/photo1/sec4.png" >
+        </div>
+        <div class="js-img2 img-2-3">
+          <img class="s5" src="./img/photo1/sec5.png" >
+        </div>
+        <div class="text text2">
+          <em>빛과 물과 눈과 얼음의 반사효과</em>
+          <i>
+            스토크크네스(Stokknes) 반도, 유명한 베스트라호른(Vestrahorn) 산
+            독특한 모양과 뾰족한 산봉우리 덕분에 “배트맨 산”이라는 이름으로 불리는 베스트라호른 산은
+            아이슬란드에서 가장 유명한 사진 촬영지 중 하나이다.
+          </i>
+        </div>
+      </div><!-- js-imgcontainer2 -->
+
+      <div class="js-imgcontainer3">
+        <div class="js-img1 img-3-1">
+          <img class="" src="./img/photo1/thir1.png" >
+          <img class="" src="./img/photo1/thir2.png" >
+          <img class="" src="./img/photo1/thir3.png" >
+        </div>
+         <div class="text text3">
+          <em>03</em>
+          <i>
+            빛이 많은 곳에서 최대한 멀어지는 것이 오로라를 선명하게 보기 위한 방법다.
+            조리개가 넓은 렌즈를 사용하는 것도 더 넓은 빛을 받을 수 있어 도움이 되며,
+            오로라가 희미하면 최대한 빛을 많이 받아야 한다.
+            셔터스피드는 느리게 하고 ISO는 높게 설정해야 한다.
+          </i>
+        </div>
+      </div><!-- js-imgcontainer3 -->
+
+
+    </figure>
+
+  </section>
+
   <section>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br>photo1<br></section>
 </section>
