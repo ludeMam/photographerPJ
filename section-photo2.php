3f8be69a48b08e4446f1577862ea89503cd2f95a
DESKTOP-KLJE6TQ\tj-bu <dudtjbestwf@naver.com>
2022-08-10 18:22:22 +0900

    8/10 이영서 작업완료

--- section-photo2.php
+++ section-photo2.php
@@ -6,7 +6,7 @@
       DARK AND MOODY & <br>
       LOW LIGHT PHOTOGRAPHY
     </h6>
-    <iframe class="video-content" src="https://www.youtube.com/embed/iFIcl4rgGhc?&playlist=iFIcl4rgGhc&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
+    <iframe class="video-content" src="https://www.youtube.com/embed/x2D7jHfitzk?&playlist=x2D7jHfitzk&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
     <img class="youtube-size-img" src="./img/icons/560x315.jpg" draggable="false" alt="">
 
     <?php include "section-photo2-svg.php" ?>
@@ -18,20 +18,24 @@
           SCENE & <br>
           LANDSCAPE
         </h6>
-        <div class="ys-imgcontainer1">
-          <div class="ys-imgcontainer1-1"><img src="./img/photo2/sub1-4.jpg" alt=""></div>
-          <div class="ys-imgcontainer1-2"><img src="./img/photo2/sub1-3.jpg" alt=""></div>
-          <div class="ys-imgcontainer1-3"><img src="./img/photo2/sub1-2.jpg" alt=""></div>
-          <div class="ys-imgcontainer1-4"><img src="./img/photo2/sub1-1.jpg" alt=""></div>
+        <div class="box1">
+          <div class="box1-1"></div>
         </div>
-        <div class="text">
-          <em>사진의 원근감과 공간표현</em>
-          <i>
+        <div class="ys-imgcontainer1">
+          <div class="img1-4"><img src="./img/photo2/sub1-4.jpg" alt=""></div>
+          <div class="img1-3"><img src="./img/photo2/sub1-3.jpg" alt=""></div>
+          <div class="img1-2"><img src="./img/photo2/sub1-2.jpg" alt=""></div>
+          <div class="img1-1"><img src="./img/photo2/sub1-1.jpg" alt=""></div>
+        </div>
+        <div class="decorbar-1"></div>
+        <div class="textbox-1">
+          <strong>사진의 원근감과 공간표현</strong>
+          <p>
           원근감이나 공간감을 사진에서 가장 쉽게 체감하는 것은 '구도'(대상의 배치)의 문제라고 생각한다.
           가장 대표적인 방법으로 소실점을 이용한 선(투시)원근법, 사물의 위치에 따른 상대적 '크기',
           사물이 겹쳐 있는 구도에서의 '중첩',그리고 색체나 명암 대비, 선명도 등을 조절하는 일명
           '공기/대기 원근법'등이 대표적이다
-          </i>
+          </p>
         </div>
       </figure>
   </section>
