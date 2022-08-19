e9a018082929356fde061c628f996debdb72dfb2
kjh <a@a.net>
2022-08-19 18:15:29 +0900

    김진형 0819 06:15

--- section-about.php
+++ section-about.php
@@ -1,7 +1,8 @@
 <section class="section-about section-container section-container2" data-section-number="2">
   <link rel="stylesheet" href="./css/section-about-1.css">
+  <link rel="stylesheet" href="./css/section-about-2.css">
   <script src="./js/section-about-1.js"></script>
-  <!--  -->
+  <!--section-video--------------------------------------------------------------->
   <a class="btn-page" href="#about" data-section=".section-about" data-section-number="2">About me</a>
   <section class="section-video-intro">
     <iframe class="video-content" src="https://www.youtube.com/embed/BHACKCNDMW8?&playlist=BHACKCNDMW8&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
@@ -14,7 +15,7 @@
       LOW LIGHT PHOTOGRAPHY
     </h6>
   </section>
-  <!--  -->
+  <!--about-section-1--------------------------------------------------------------->
   <section class="yj-about-section-1">
     <div class="inner-center">
       <img class="deco" src="./img/aboutme/visualMaskbg.png" alt="">
@@ -35,10 +36,47 @@
         </p>
       </div>
     </div><!-- inner-center -->
-  </section><!-- section1 -->
-
-
-  <section>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+  </section>
+  <!--about-section-2--------------------------------------------------------------->
+  <section class="yj-about-section-2">
+    <h2 class="hidden">identify</h2>
+    <div class="inner-center clearfix">
+      <div class="textbox textbox1">
+        <h3>What is Lorem Ipsum?</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+      </div><!--textbox-->
+      <div class="textbox textbox1">
+        <h3>Why do we use it?</h3>
+        <p>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+        </p>
+      </div><!--textbox-->
+      <div class="textbox textbox3">
+        <h3>What is Lorem Ipsum?</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+      </div><!--textbox-->
+      <div class="textbox textbox4">
+        <h3>Why do we use it?</h3>
+        <p>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+        </p>
+      </div><!--textbox-->
+      <div class="textbox textbox5">
+        <h3>What is Lorem Ipsum?</h3>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </p>
+      </div><!--textbox-->
+      <div class="textbox textbox6">
+        <h3>Why do we use it?</h3>
+        <p>
+        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+        </p>
+      </div><!--textbox-->
+    </div><!-- inner-center -->
   </section>
 </section>
