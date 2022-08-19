49174d9fce362d7a19dd1c388836c13b45c9bb10
kjh <a@a.net>
2022-08-19 17:07:48 +0900

    section-about-1,video-mask-img 김진형 0819 05:07

--- section-about.php
+++ section-about.php
@@ -1,21 +1,33 @@
 <section class="section-about section-container section-container2" data-section-number="2">
-  <link rel="stylesheet" href="./css/section-about.css">
+  <link rel="stylesheet" href="./css/section-about-1.css">
   <script src="./js/section-about-1.js"></script>
   <!--  -->
   <a class="btn-page" href="#about" data-section=".section-about" data-section-number="2">About me</a>
   <section class="section-video-intro">
     <iframe class="video-content" src="https://www.youtube.com/embed/BHACKCNDMW8?&playlist=BHACKCNDMW8&vq=highres&iv_load_policy=3&autoplay=1&mute=1&loop=1&rel=0&showinfo=0&color=white&controls=0" allowfullscreen></iframe>
     <img class="youtube-size-img" src="./img/icons/560x315.jpg" draggable="false" alt="">
-    <img class="white-cover" src="./img/icons/white-cover.png" alt="">
+    <img class="video-mask-img" src="./img/icons/video-mask-about.png" alt="">
+    <?php //include "section-photo2-svg.php" 
+    ?>
+    <h6>
+      DARK AND MOODY & <br>
+      LOW LIGHT PHOTOGRAPHY
+    </h6>
   </section>
   <!--  -->
   <section class="yj-about-section-1">
     <div class="inner-center">
+      <img class="deco" src="./img/aboutme/visualMaskbg.png" alt="">
       <figure class="about-me-figure">
         <img class="bg" src="./img/aboutme/visualImg1.png" alt="">
         <img class="man" src="./img/aboutme/visualImg2.png" alt="">
       </figure>
+
       <div class="textbox">
+        <figure class="textbox-bg">
+          <img class="bg" src="./img/aboutme/profile4-bg.jpg" alt="">
+          <img class="man" src="./img/aboutme/profile4-man.png" alt="">
+        </figure>
         <img class="photo" src="./img/aboutme/man.png" alt="">
         <h2><span>about me</span></h2>
         <p class="icon-font">
