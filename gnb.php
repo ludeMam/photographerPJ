8d25baeb525bc36ab242ffd567f38023c13fc7e7
DESKTOP-KLJE6TQ\tj-bu <dudtjbestwf@naver.com>
2022-08-22 18:15:59 +0900

    8/22 이영서 main&gnb 작업완료

--- gnb.php
+++ gnb.php
@@ -1,9 +1,23 @@
-<button class="btn-menu">Menu</button>
+
 
 <nav class="gnb">
-  <a class="gnbNA" href="#home" data-section=".section-home" data-section-number="1">Home</a>
-  <a class="gnbNA" href="#about" data-section=".section-about" data-section-number="2">About me</a>
-  <a class="gnbNA" href="#photo1" data-section=".section-photo1" data-section-number="3">Bright moody</a>
-  <a class="gnbNA" href="#photo2" data-section=".section-photo2" data-section-number="4">Dark moody</a>
-  <a class="gnbNA" href="#contact" data-section=".section-contact" data-section-number="5">Contact</a>
+
+  <h6>siteTitle</h6>
+  <ul>
+    <li><a class="gnbNA" href="#home" data-section=".section-home" data-section-number="1">Home</a></li>
+    <li><a class="gnbNA" href="#about" data-section=".section-about" data-section-number="2">About me</a></li>
+    <li><a class="gnbNA" href="#photo1" data-section=".section-photo1" data-section-number="3">Bright moody</a></li>
+    <li><a class="gnbNA" href="#photo2" data-section=".section-photo2" data-section-number="4">Dark moody</a></li>
+    <li><a class="gnbNA" href="#contact" data-section=".section-contact" data-section-number="5">Contact</a></li>
+  </ul>
+  <ul class="gnb-sns">
+    <li>Follow us</li>
+    <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a></li>
+    <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+    <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+  </ul>
+  
+  
+  
+  
 </nav>
--- main.php
+++ main.php
@@ -1,10 +1,16 @@
 <script src="./js/main&gnb.js"></script>
 <main id="style-4">
+  <?php include "gnb.php"?>
   <?php include "particle.php" ?>
   <?php include "section-home.php" ?>
   <?php include "section-about.php" ?>
   <?php include "section-photo1.php" ?>
   <?php include "section-photo2.php" ?>
   <?php include "section-contact.php" ?>
+  <button class="btn-menu">
+    <span class="circle" aria-hidden="true">
+      <span class="icon arrow"></span>
+    </span>
+    <span class="button-text">MENU</span>
+  </button>
 </main>
-<?php include "gnb.php"?>
