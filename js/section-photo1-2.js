6c0f30d608d94378295b2d4201c6369f88311b92
DESKTOP-IQG9OCR\KJH <kjhardcore@naver.com>
2022-08-29 20:52:13 +0900

    0829 0852

--- js/section-photo1-2.js
+++ js/section-photo1-2.js
@@ -15,17 +15,7 @@ $(function(){
   var imgRangeY
 
   $('ul.main-content li:first-child').each(function(){
-    var src = $(this).find('.original img').attr('src')
-    var imgCnt = 10
-    for(i=1;i<=imgCnt;i++){
-      $(this).find('.zoom-lg').append('<img class="zoom-lg-img-'+i+'" src="'+src+'">')
-      /* resize */
-      var w = $(this).find('.original img').width()
-      var imgw = w + (i - 1) * (w / imgCnt) 
-      var mskw = w - (i - 1) * (w / imgCnt)
-      $('.zoom-lg-img-'+i).css({'width':+imgw+'px','height':imgw + 'px','clip-path':'circle('+mskw+'px)'}) 
 
-    }//for
   })
 
   $('ul.main-content li .original').mousemove(function(e){
