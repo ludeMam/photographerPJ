8e18e5d3ff903c01f3e0e86c4e76424827fb8da7
bluehalu98 <bluehalu98@gmail.com>
2022-08-29 18:20:34 +0900

    8/29 진영진 작업완료 (학원)

--- section-about.php
+++ section-about.php
@@ -16,6 +16,107 @@
   </section>
   <!--about-section-1--------------------------------------------------------------->
   <section class="ha-section-about-1">
-    about-section-1
+    <h2 class="hidden">about me</h2>
+    <section class="introduce">
+      <h3>emma roberts</h3>
+      <p>landscape photographer</p>
+      <figure>
+        <img src="./img/about/photographer.png" alt="">
+      </figure>
+    </section><!-- introduce section -->
+    <section class="main-content">
+      <div class="experience-content">
+        <h3>experience</h3>
+        <ul>
+          <li>
+            <h4>2015 - Present</h4>
+            <h5>yellow man advertising</h5>
+            <div>
+              <span>landscape photographic director</span>
+              <p>Brief description of the position and the responsiblities you had in this post.</p>
+            </div>
+          </li>
+          <li>
+            <h4>2012 - 2015</h4>
+            <h5>big bang studios</h5>
+            <div>
+              <span>senior landscape photographic designer</span>
+              <p>Brief description of the position and the responsiblities you had in this post.</p>
+            </div>
+          </li>
+          <li>
+            <h4>2010 - 2012</h4>
+            <h5>high jump studios</h5>
+            <div>
+              <span>landscape photographic designer</span>
+              <p>Brief description of the position and the responsiblities you had in this post.</p>
+            </div>
+          </li>
+          <li>
+            <h4>2008 - 2010</h4>
+            <h5>junoir photographer</h5>
+            <div>
+              <span>landscape photographer</span>
+              <p>Brief description of the position and the responsiblities you had in this post.</p>
+            </div>
+          </li>
+        </ul>
+      </div><!-- experience section -->
+      <div class="education-content">
+        <h3>education</h3>
+        <ul>
+          <li>
+            <h4>2011 - 2013</h4>
+            <h5>ma advertising</h5>
+            <p>university of london, uk</p>
+          </li>
+          <li>
+            <h4>2007 - 2011</h4>
+            <h5>ba graphic design</h5>
+            <p>london college of photographic, uk</p>
+          </li>
+          <li class="skills-brief">
+            <h4>skills</h4>
+            <ul>
+              <li>
+                <h5>photoshop</h5>
+                <div></div>
+              </li>
+              <li>
+                <h5>illustrator</h5>
+                <div></div>
+              </li>
+              <li>
+                <h5>indesign</h5>
+                <div></div>
+              </li>
+              <li>
+                <h5>microsoft office</h5>
+                <div></div>
+              </li>
+              <li>
+                <h5>dreamweaver</h5>
+                <div></div>
+              </li>
+              <li>
+                <h5>html5</h5>
+                <div></div>
+              </li>
+              <li>
+                <h5>css3</h5>
+                <div></div>
+              </li>
+            </ul><!-- skills brief -->
+          </li>
+          <li>
+            <h4>contact</h4>
+            <p>
+              emmaroberts@myportfolio.com
+              +44 20 7946 0614
+            </p>
+          </li>
+        </ul>
+      </div><!-- education content -->
+    </section><!-- main content section -->
   </section>
 </section><!--section-about-container-->
