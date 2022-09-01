2b2d178b3b162b4341eb922a016efc293e177558
bluehalu98 <bluehalu98@gmail.com>
2022-09-01 11:38:30 +0900

    9/1 진영진 푸터 작업중 (집)

--- footer.php
+++ footer.php
@@ -1,8 +1,35 @@
 <!-- <div class="footer-height"></div> -->
 <footer class="site-footer">
-  <div class="footer-photographer"></div>
-  <div class="footer-site-map"></div>
-  <div class="footer-sns"></div>
-</footer>
+  <div class="footer-container">
+    <div class="footer-site-map">
+      <h4>site</h4>
+      <ul>
+        <li><a href="">home</a></li>
+        <li><a href="">bright mood</a></li>
+        <li><a href="">dark mood</a></li>
+        <li><a href="">contact</a></li>
+      </ul>
+    </div>
+    <div class="footer-link">
+      <div class="footer-contact">
+        <h4>contact</h4>
+        <address>
+          <i>1120 Lorem ipsum dolor sit amet, KC 179050, Chandigarh.</i>
+          <i>+01 2345 6789 12</i>
+          <i>emma.roberts@gmail.com</i>
+        </address>
+        <button>more info</button>
+      </div>
+      <ul class="footer-sns">
+        <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+      </ul>
+    </div>
+  </div><!-- footer container -->
+  <p class="footer-copyright">
+    Copyright &copy; 2022 All Rights Reserved.
+  </p><!-- footer copyright -->
+</footer><!-- footer -->
 </body>
 </html>
